3af22164e08ddbfd9da21641f6fb7fd94e8a52ad
jed4739 <tjsdlr04@gmail.com>
2022-12-26 11:13:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.webview.databinding.Main;
+
+public class MainActivity extends AppCompatActivity {
+    Main binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.webView.loadUrl("https://m.nbkorea.com/index.action");
+    }
+}
